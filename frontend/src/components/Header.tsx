b71386046973a1f8b73f806285124e8f7b0bd1b9
suzukiyashiro <j2100095@gunma-u.ac.jp>
2024-10-29 18:47:51 +0900

    Headerの作成

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <div className="my-8 text-center text-4xl font-bold text-blue-500">
+      Fanza App
+    </div>
+  );
+};
 
-export default Header
+export default Header;
